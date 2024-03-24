cff3ed17e00f62b22c5e8e832bb12dd12ba175c0
hadasbenisty <hadas.benisty@gmail.com>
2024-03-24 09:43:08 +0200

    Update main.py

--- template/main.py
+++ template/main.py
@@ -2,34 +2,13 @@ import os
 from c_stg.train_main import main_workflow
 
 if __name__ == '__main__':
-    # from flavors.utils import get_subdirectories
-    # data_directory = '..\\data'
-    # for animal in os.listdir(data_directory):
-    #     print(animal)
-    #     if animal != '4575' and animal != 'animals_db_selected.xlsx' and animal != '4458' and animal != '4754' and animal != '4756':
-    #         animal_directory = data_directory + "\\" + animal
-    #         dates = get_subdirectories(animal_directory)
-    #         if animal == '4754':
-    #             dates = dates[1:]
-    #         for date in dates:
-    #             print(f"WORKING ON ANIMAL:{animal},DATE:{date}")
-    #             arguments_dict = {'animal': animal, 'date': date}
-    #             main_workflow(**arguments_dict)
 
-    from flavors.utils import get_subdirectories
+
+    from template.utils import get_subdirectories
     data_directory = '..\\data'
-    #animals = ['1111', '4458']
-    animals = ['1111']
-    for animal in animals:
-        animal_directory = data_directory + "\\" + animal
-        if animal == '4458':
-            dates = get_subdirectories(animal_directory)
-            dates = [dates[2]]
-        elif animal == '1111':
-            dates = ['01_20_24']
-        for date in dates:
-            print(f"WORKING ON ANIMAL:{animal}, DATE:{date}")
-            data_dict = {'animal': animal, 'date': date}
+    specific_data = "my_data
+    print(f"WORKING ON ANIMAL:{specific_data}")
+            data_dict = {'specific_data': specific_data}
             #cstg_dict = {'hyper_hidden_dims': 0}
 
-            main_workflow(data_type='flavors', cstg_args={}, data_args=data_dict)
+            main_workflow(data_type='template', cstg_args={}, data_args=data_dict)
