15766d24d5a0a8ca2f101f882ffad100b50cfee4
hadasbenisty <hadas.benisty@gmail.com>
2024-03-24 09:31:54 +0200

    Update data_params.py

--- template/data_params.py
+++ template/data_params.py
@@ -7,30 +7,24 @@ class Data_params(object):
         # Data Parameters #
         ######################
         # ---- main user input ----
-        self.animal = '4458'  # mouse number
-        self.date = '03_05_19'  # date of experiment
-        self.outcome_keys = ['success']  # ['success'] all the outcomes to consider
-        self.context_key = 'time'
+        #self.(specific fields for the data) = ...
+        # define label for prediction                
+        self.outcome_keys = ['success']  # replace with you own label
+        # define label for context
+        self.context_key = 'time' #replace time with your own context
+        // add your personal documentation
         self.note = "new model, 4458 with flavors context and success classification, 4-8 time, with 40 times same acc until run again"  # save in the log context for self use notes
         # ---- post processing user input ----
         self.manual_random_seed = -1  # seed for not determensric operations
         # -----------------------------------
-        self.mat_files_directory = '../data/'  # 'D:\\flavorsProject\data\\'  # '/home/shiralif/data/'
-        self.result_directory = '../results/'  # '..\\results\\'  # '/home/shiralif/results/'
-        self.info_excel_path = '../data/animals_db_selected.xlsx'  # '..\\data\\animals_db_selected.xlsx'  # '/home/shiralif/data/animals_db_selected.xlsx'
-        self.outcome_options = ['success', 'fake', 'grain', 'sucrose', 'quinine', 'flavors']  # labels options, flavors include all the successful tastes and the failure
-        self.context_options = ['time', 'flavors']
-        self.animal2sheet_num = {'4458': 0, '4575': 1, '4754': 2,
-                                 '4756': 3, '4880': 4, '4882': 5, '4940': 6, '1111': 7}  # excel sheet num
-        self.flavors2num = {'g': 1, 's': 2, 'q': 3, 'r': 4, 'f': 5, 'fail': 0}
-        self.folds_num = 5  # cross validation
-        self.start_time = -4  # sec
-        self.end_time = 8  # sec
-        self.drop_time = 1  #8  # usually drop the first sec #Todo
-        self.total_time = -1  #4 # -1 for all the data duration, else number of sec #Todo
-        self.sample_per_sec = 30  # samples/sec
-        self.window_size_avg = 1  # sec
-        self.overlap_avg = 0.5  # sec
+        self.mat_files_directory = '../data/'  # replace with you own 
+        self.result_directory = '../results/'  # replace with you own 
+        self.info_excel_path = '../data/animals_db_selected.xlsx'  # replace with you own if relevant
+        self.outcome_options = ['success', 'fake', 'grain', 'sucrose', 'quinine', 'flavors']  # replace with you own 
+        self.context_options = ['context1', 'context2'] # replace with your context labels
+        
+        self.folds_num = 5  # cross validation  replace with you own 
+       
 
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -38,20 +32,22 @@ class Data_params(object):
 
 def data_origanization_params(params):
     if params.post_process_mode:
+        # example code for the way output folder is extracted for flavors project. do this in a later stage
+        
+        # animal_idx = params.infer_directory.find('animal')
+        # animal_idx = animal_idx + len('animal') + 1
+        # params.animal = params.infer_directory[animal_idx:].split('_')[0]
 
-        animal_idx = params.infer_directory.find('animal')
-        animal_idx = animal_idx + len('animal') + 1
-        params.animal = params.infer_directory[animal_idx:].split('_')[0]
+        # date_idx = params.infer_directory.find('date')
+        # date_idx = date_idx + len('date') + 1
+        # params.date = '_'.join(params.infer_directory[date_idx:].split('_')[0:3])
 
-        date_idx = params.infer_directory.find('date')
-        date_idx = date_idx + len('date') + 1
-        params.date = '_'.join(params.infer_directory[date_idx:].split('_')[0:3])
-
-        outcome_keys_idx = params.infer_directory.find(params.date)
-        outcome_keys_idx = outcome_keys_idx + len(params.date) + 1
-        params.outcome_keys = (params.infer_directory[outcome_keys_idx:]).split('_')
+        # outcome_keys_idx = params.infer_directory.find(params.date)
+        # outcome_keys_idx = outcome_keys_idx + len(params.date) + 1
+        # params.outcome_keys = (params.infer_directory[outcome_keys_idx:]).split('_')
 
     else:
+        # example code for the way output folder is determined for flavors project. change this according to the specific experiment 
         running_datetime = datetime.now()
         running_datetime = running_datetime.strftime("%Y_%m_%d_%H_%M_%S")
         sub_res_directory = running_datetime + '_animal_' + params.animal + '_date_' + params.date + '_' + '_'.join(params.outcome_keys)
