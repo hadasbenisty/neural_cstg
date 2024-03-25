795b94b0094a3963b86877a97e8d7a311ab66ba1
NoNameLand <ariel.engelman1310@gmail.com>
2024-03-25 19:14:08 +0200

    Update main.py

--- GaligoolAngel/main.py
+++ GaligoolAngel/main.py
@@ -7,9 +7,9 @@ import scipy.io as spio
 # Calling the main workflow
 our_folder = 'GaligoolAngel'
 cstg_params = {'ML_model_name': 'fc_stg_layered_param_modular_model_sigmoid',
-               'classification_flag': False, 'folds_num': 5, 'hidden_dims': [[900,90], [500, 120],[200,100]],
-               'learning_rates': [0.0005, 0.0001, 0.001],
-               'num_epoch': 150, 'hyper_hidden_dims': [[50, 500],[30,500],[10,300],[100,1000],[100]], 'stg_regularizers': [0.1, 1]}
+               'classification_flag': False, 'folds_num': 5, 'hidden_dims': [[200, 100]], # [900,90], [500, 120],[200,100]
+               'learning_rates': [0.0005], # 0.0005, 0.0001, 0.001 
+               'num_epoch': 150, 'hyper_hidden_dims': [[30, 500]], 'stg_regularizers': [0.1]} # [50, 500],[30,500],[10,300],[100,1000],[100] stg 1
 data_params = {'matfile_path': 'C:/Users/hadas-stud-group2/Documents/GitHub/neural_cstg/GaligoolAngel/data/inputs/dataset_diff_animal2.mat'}
 result_path = train_main.main_workflow(cstg_args=cstg_params, data_type=our_folder, data_args=data_params)
 
@@ -21,11 +21,16 @@ raw_data = spio.loadmat(data_params["matfile_path"])
 cc = raw_data["CC"]
 
 # Taking Subset Of Features
-cc_analysis = cc[results_processor.dynamic_neurons, results_processor.dynamic_neurons, :]
-analyzer = ResultAnalyzer(cc_analysis)
-
+cc_dynamic_analysis = cc[results_processor.dynamic_neurons, results_processor.dynamic_neurons, :]
+analyzer = ResultAnalyzer(cc_dynamic_analysis)
 analyzer.community_analysis()
 analyzer.degree_analysis()
 analyzer.eigen_values_analysis()
 analyzer.plot_analysis_results("all")
 
+cc_important_analysis = cc[results_processor.important_neurons, results_processor.important_neurons, :]
+analyzer_imp = ResultAnalyzer(cc_important_analysis)
+analyzer.community_analysis()
+analyzer.degree_analysis()
+analyzer.eigen_values_analysis()
+analyzer.plot_analysis_results("all")
